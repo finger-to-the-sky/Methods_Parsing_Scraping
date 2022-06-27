f44e2ee84135d08937523104a4327166332f7276
finger_to_the_sky <mzagrebam@gmail.com>
2022-06-27 18:21:36 +0300

    add function add_on_mongo

---
+++ Lesson_3/add_on_mongo.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+# для использования функции я сделаю импорт из предыдущего урока
+from Lesson_2.task_1 import total_result
+
+
+def add_on_mongo(MONGO_HOST, MONGO_PORT, MONGO_DB, MONGO_COLLECTION):
+    with MongoClient(MONGO_HOST, MONGO_PORT) as client:
+        vacanties = total_result.run()
+        db = client[MONGO_DB]
+        col_vacanties = db[MONGO_COLLECTION]
+
+        for vacancy in vacanties:
+
+            col_vacanties.insert_one(vacancy)
+
+    print(f'Добавлено {len(vacanties)} новых вакансий')
